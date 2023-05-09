2ab3111605df059d748a2e9311d17ab305c4c011
PrinceSharma <121371635+thisis-princeSharma@users.noreply.github.com>
2023-05-08 23:42:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+function bor() {
+	let G = document.getElementById("bor");
+	G.style.width = "235px";
+}
+function bor2() {
+	let G = document.getElementById("bor");
+	G.style.width = "0px";
+}
+function trio1() {
+	let Q = document.getElementById("tri2");
+	Q.style.width = "45px";
+}
+function tric1() {
+	let W = document.getElementById("tri2");
+	W.style.width = "0px";
+}
+function trio2() {
+	let Q = document.getElementById("tri3");
+	Q.style.width = "60px";
+}
+function tric2() {
+	let W = document.getElementById("tri3");
+	W.style.width = "0px";
+}
+function trio3() {
+	let Q = document.getElementById("tri");
+	Q.style.width = "45px";
+}
+function tric3() {
+	let W = document.getElementById("tri");
+	W.style.width = "0px";
+}
+function bor3() {
+	let G = document.getElementById("bor1");
+	G.style.width = "200px";
+}
+function bor4() {
+	let G = document.getElementById("bor1");
+	G.style.width = "0px";
+}
+
+function under() {
+	let under = document.getElementById("under");
+	under.style.width = "225px";
+}
+function under2() {
+	let under = document.getElementById("under");
+	under.style.width = "0px";
+}
+
+function under3() {
+	let underser = document.getElementById("underser");
+	underser.style.width = "235px";
+}
+function under4() {
+	let underser = document.getElementById("underser");
+	underser.style.width = "0px";
+}
+
+function tric6() {
+	let W = document.getElementById("tri4");
+	W.style.width = "0px";
+}
+function trio7() {
+	let Q = document.getElementById("tri4");
+	Q.style.width = "60px";
+}
+
+function openNav() {
+	document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+}
+
+function hire_link() {
+	window.open("https://www.linkedin.com/in/prince-sharma-966579266/", "_blank");
+}
+
+
+
